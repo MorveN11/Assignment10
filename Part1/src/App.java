3828ce51f9f60c67c844d14f08051af7297bc8af
MorveN11 <morales.patty.jose@gmail.com>
2022-03-13 17:17:19 -0400

    prueba

--- Part1/src/App.java
+++ Part1/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         Player player = new Player();
         System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
