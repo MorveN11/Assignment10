7fcd931a4fbfdc8fa76056dae9b36dc45055a69b
Miguel2774 <pre100503@gmail.com>
2022-03-14 12:50:37 -0400

    hONGuito

--- Part1/src/Items/Consumables/Trap/Mana/Types/MushRoom.java
+++ Part1/src/Items/Consumables/Trap/Mana/Types/MushRoom.java
@@ -3,14 +3,28 @@ package Items.Consumables.Trap.Mana.Types;
 import Items.Player;
 import Items.Consumables.Trap.Mana.ManaTrapConsumable;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MushRoom extends ManaTrapConsumable {
     Object player;
     public MushRoom (Object player) {
         this.player = (Player) player;
-        setHurt(100);
         setTrapTypeConsumable("MushRoom");
     }
     public void silenceMana() {
-        //Silencea al jugador por un tiempo determinado
+        Timer timer = new Timer();
+        int previousMana = ((Player) player).getMana();
+        System.out.println("Tu mana será 1 por 10 segundos cuidado");
+        ((Player) player).setMana(1);
+        TimerTask work = new TimerTask() {
+            @Override
+            public void run() {
+                ((Player) player).setMana(previousMana);
+                System.out.println(((Player) player).getMana());
+            }
+        };
+        System.out.println(((Player) player).getMana());
+        timer.schedule(work, 10000);
     }
 }
