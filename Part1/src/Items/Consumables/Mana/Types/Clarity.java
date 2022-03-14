85d87fe3a944a124020176a1a648e9c3002a43df
Diego Perez <diegoguitarra64@gmail.com>
2022-03-14 11:57:44 -0400

    Update Clarity Items 1
    implement timer

--- Part1/src/Items/Consumables/Mana/Types/Clarity.java
+++ Part1/src/Items/Consumables/Mana/Types/Clarity.java
@@ -3,23 +3,31 @@ package Items.Consumables.Mana.Types;
 import Items.Player;
 import Items.Consumables.Mana.ManaConsumable;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Clarity extends ManaConsumable {
     Object player;
     public Clarity(Object player) {
         this.player = (Player) player;
-        setCuration(150);
+        setCuration(250);
         setManaTypeConsumable("Clarity");
     }
     public void progressiveRestoringMana() {
+        Timer timer = new Timer();
         if(((Player)player).getMana() + getCuration() > ((Player)player).getMaxMana()) {
             setCuration(((Player)player).getMaxMana() - ((Player)player).getMana());
         }
-
-        for (int i = 0; i < getCuration(); i++)
-        {
-            ((Player)player).setMana(((Player)player).getMana() + 1);
-            System.out.println(((Player)player).getMana());
-        //nos cura mana de forma progresiva
-    }
+        TimerTask work = new TimerTask() {
+            @Override
+            public void run() {
+                if(getCuration() > 0) {
+                    ((Player) player).setMana(((Player) player).getMana() + 1);
+                    setCuration(getCuration() - 1);
+                    System.out.println(((Player) player).getMana());
+                }
+            }
+        };
+        timer.schedule(work, 0, 500);
     }
 }
